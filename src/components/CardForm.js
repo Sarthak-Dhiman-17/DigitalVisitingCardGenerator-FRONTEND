f2c76a78aabe19c5ad4bac163ef9760e8c9f84ff
Sarthak-Dhiman-17 <127050804+Sarthak-Dhiman-17@users.noreply.github.com>
2025-07-13 19:41:50 +0530

    Connecting frontend to backend

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -9,7 +9,7 @@ function CardForm({ cardData, setCardData }) {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            await axios.post('http://localhost:8080/api/cards', cardData);
+            await axios.post(`${process.env.REACT_APP_API_URL}/api/cards`, data);
             alert('✅ Card submitted successfully!');
         } catch (err) {
             console.error(err);
